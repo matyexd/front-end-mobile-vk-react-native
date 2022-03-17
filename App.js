5f03251cc05bf35340f6fbb4bc7f686b2195d90a
Matvey Mamochkin <matv.softgreed@gmail.com>
2022-03-17 17:48:46 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text style={styles.title}>Привет</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    color: 'black',
+    fontSize: 40,
+    fontFamily: 'hk_grotesk_medium',
+  },
+});
+
+export default App;
